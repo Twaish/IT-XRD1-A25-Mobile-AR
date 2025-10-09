64f6b44a0c9a5e400d7c3222c72a8d89fe51a592
Twaish <axiomeverything@gmail.com>
2025-10-09 15:35:14 +0200

    Add script for applying leaves effects from weather data

---
+++ Assets/Scripts/WeatherConditions/Leaves/LeavesManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+[RequireComponent(typeof(LeavesEffectHandler))]
+public class LeavesManager : MonoBehaviour
+{
+  [SerializeField] private WeatherService weatherService;
+  private LeavesEffectHandler leavesEffectHandler;
+
+  private void Awake()
+  {
+    leavesEffectHandler = GetComponent<LeavesEffectHandler>();
+    if (leavesEffectHandler == null)
+    {
+      Debug.LogError("LeavesManager: LeavesEffectHandler not found");
+      enabled = false;
+      return;
+    }
+
+    if (weatherService != null)
+    {
+      weatherService.OnWeatherUpdated += OnWeatherUpdated;
+    }
+    else
+    {
+      Debug.LogError("LeavesManager: WeatherService instance not found");
+      enabled = false;
+    }
+  }
+
+  private void OnDestroy()
+  {
+    if (weatherService != null)
+    {
+      weatherService.OnWeatherUpdated -= OnWeatherUpdated;
+    }
+  }
+
+  private void OnWeatherUpdated(WeatherResponse weather)
+  {
+      if (weather == null) return;
+
+      float windSpeed = weather.current.wind_kph;
+      float windDegree = weather.current.wind_degree;
+
+      if (windSpeed > 0.01f)
+      {
+          leavesEffectHandler.SetActive(true);
+          leavesEffectHandler.SetLeavesRate(windSpeed);
+          leavesEffectHandler.SetLeavesVelocity(windSpeed, windDegree);
+      }
+      else
+      {
+          leavesEffectHandler.SetActive(false);
+      }
+  }
+  
+}
