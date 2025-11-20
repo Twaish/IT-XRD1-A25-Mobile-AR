4dbdc09475e02b3de805983ce65e0107d65817ce
JonasPHenriksen <jonashenriksenprivat@gmail.com>
2025-11-20 16:10:15 +0100

    Windspeed slightly lower

--- Assets/Scripts/WeatherConditions/Wind/WindManager.cs
+++ Assets/Scripts/WeatherConditions/Wind/WindManager.cs
@@ -42,7 +42,7 @@ public class WindManager : MonoBehaviour
     float windSpeed = weather.current.wind_kph;
     float windDegree = weather.current.wind_degree;
 
-    if (windSpeed > 0.8f)
+    if (windSpeed > 0.5f)
     {
       windEffectHandler.SetActive(true);
       windEffectHandler.SetWind(windSpeed, windDegree);
